c7442ca3e9953cc5f394f4a7278cd1480c1dfe75
Stepp1 <sfaragg@gmail.com>
2021-05-02 23:53:25 -0400

    formatting

--- t1/datasets/create_tfrecords.py
+++ t1/datasets/create_tfrecords.py
@@ -12,27 +12,42 @@ import utils.configuration as conf
 import utils.imgproc as imgproc
 import datasets.data as data
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Create a dataset for training an testing")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create a dataset for training an testing"
+    )
     """ pathname must include train.txt and test.txt files """
-    parser.add_argument("-type", type=str, help="train| test | all", choices=['train', 'test', 'all'], required=True)
-    parser.add_argument("-config", type=str, help="<str> configuration file", required=True)
-    parser.add_argument("-name", type=str, help=" name of section in the configuration file", required=True)
+    parser.add_argument(
+        "-type",
+        type=str,
+        help="train| test | all",
+        choices=["train", "test", "all"],
+        required=True,
+    )
+    parser.add_argument(
+        "-config", type=str, help="<str> configuration file", required=True
+    )
+    parser.add_argument(
+        "-name",
+        type=str,
+        help=" name of section in the configuration file",
+        required=True,
+    )
     pargs = parser.parse_args()
     configuration_file = pargs.config
     # assert os.path.exists(configuration_file), "configuration file does not exist {}".format(configuration_file)
     configuration = conf.ConfigurationFile(configuration_file, pargs.name)
 
     process_fun = imgproc.process_image
-    if configuration.get_image_type() == 'SKETCH':
+    if configuration.get_image_type() == "SKETCH":
         process_fun = imgproc.process_sketch
-    elif configuration.get_image_type() == 'MNIST':
+    elif configuration.get_image_type() == "MNIST":
         process_fun = imgproc.process_mnist
-    elif configuration.get_image_type() == 'FASHION':
+    elif configuration.get_image_type() == "FASHION":
         process_fun = imgproc.process_image
-    elif configuration.get_image_type() == 'CUSTOM':
-        process_fun = imgproc.create_processing_function(configuration.get_image_processing_params())
-    data.create_tfrecords(configuration,
-                          pargs.type,
-                          processFun=process_fun)
+    elif configuration.get_image_type() == "CUSTOM":
+        process_fun = imgproc.create_processing_function(
+            configuration.get_image_processing_params()
+        )
+    data.create_tfrecords(configuration, pargs.type, processFun=process_fun)
     print("tfrecords created for " + configuration.get_data_dir())
--- t1/datasets/data.py
+++ t1/datasets/data.py
@@ -48,17 +48,17 @@ def read_image(filename, number_of_channels):
     if number_of_channels == 1:
         image = io.imread(filename, as_gray=True)
         image = imgproc.toUINT8(image)
-        assert (len(image.shape) == 2)
+        assert len(image.shape) == 2
         image = np.expand_dims(image, axis=2)  # H,W,C
-        assert (len(image.shape) == 3 and image.shape[2] == 1)
+        assert len(image.shape) == 3 and image.shape[2] == 1
     elif number_of_channels == 3:
         image = io.imread(filename)
-        if (len(image.shape) == 2):
+        if len(image.shape) == 2:
             image = color.gray2rgb(image)
         elif image.shape[2] == 4:
             image = color.rgba2rgb(image)
         image = imgproc.toUINT8(image)
-        assert (len(image.shape) == 3 and image.shape[2] == 3)
+        assert len(image.shape) == 3 and image.shape[2] == 3
     else:
         raise ValueError("number_of_channels must be 1 or 3")
     if not os.path.exists(filename):
@@ -90,13 +90,15 @@ def read_data_from_file(str_path, dataset="train", shuf=True):
         lines = [line.rstrip() for line in file]
         if shuf:
             random.shuffle(lines)
-        _lines = [tuple(line.rstrip().split('\t')) for line in lines]
+        _lines = [tuple(line.rstrip().split("\t")) for line in lines]
         filenames, labels = zip(*_lines)
         labels = validate_labels(labels)
     return filenames, labels
 
 
-def create_tfrecords_from_file(filenames, labels, image_shape, tfr_filename, process_function=imgproc.resize_image):
+def create_tfrecords_from_file(
+    filenames, labels, image_shape, tfr_filename, process_function=imgproc.resize_image
+):
     # create tf-records
     writer = tf.io.TFRecordWriter(tfr_filename)
     # filenames and lables should  have the same size
@@ -114,8 +116,10 @@ def create_tfrecords_from_file(filenames, labels, image_shape, tfr_filename, pro
             # print(" {} {} ".format(image.shape, labels[i]))
             # cv2.waitKey()
             # create a feature
-            feature = {'image': _bytes_feature(tf.compat.as_bytes(image.tostring())),
-                       'label': _int64_feature(labels[i])}
+            feature = {
+                "image": _bytes_feature(tf.compat.as_bytes(image.tostring())),
+                "label": _int64_feature(labels[i]),
+            }
 
             # create an example protocol buffer
             example = tf.train.Example(features=tf.train.Features(feature=feature))
@@ -131,11 +135,18 @@ def create_tfrecords_from_file(filenames, labels, image_shape, tfr_filename, pro
     return mean_image
 
 
-def process_batch_threads(thr_index, ranges, filenames, labels, image_shape, tfr_filename,
-                          process_function=imgproc.resize_image):
+def process_batch_threads(
+    thr_index,
+    ranges,
+    filenames,
+    labels,
+    image_shape,
+    tfr_filename,
+    process_function=imgproc.resize_image,
+):
     # create tf-records
-    tfr_filename_batch = '{}_{}.tfrecords'.format(tfr_filename, thr_index)
-    mean_filename_batch = '{}_{}_mean.npy'.format(tfr_filename, thr_index)
+    tfr_filename_batch = "{}_{}.tfrecords".format(tfr_filename, thr_index)
+    mean_filename_batch = "{}_{}_mean.npy".format(tfr_filename, thr_index)
     writer = tf.io.TFRecordWriter(tfr_filename_batch)
     # filenames and lables should  have the same size
     assert len(filenames) == len(labels)
@@ -147,29 +158,40 @@ def process_batch_threads(thr_index, ranges, filenames, labels, image_shape, tfr
         try:
             image = read_image(filenames[idx], image_shape[2])  # scikit-image
             image = process_function(image, (image_shape[0], image_shape[1]))
-            feature = {'image': _bytes_feature(tf.compat.as_bytes(image.tostring())),
-                       'label': _int64_feature(labels[idx])}
+            feature = {
+                "image": _bytes_feature(tf.compat.as_bytes(image.tostring())),
+                "label": _int64_feature(labels[idx]),
+            }
             # create an example protocol buffer
             example = tf.train.Example(features=tf.train.Features(feature=feature))
             # serialize to string and write on the file
             writer.write(example.SerializeToString())
-            mean_batch = mean_batch + image / batch_size;
+            mean_batch = mean_batch + image / batch_size
             count = count + 1
             if count % 100 == 0:
                 print(
-                    '{} Thread {} --> processing {} of {} [{}, {}]'.format(datetime.now(), thr_index, count, batch_size,
-                                                                           ranges[thr_index][0], ranges[thr_index][1]))
+                    "{} Thread {} --> processing {} of {} [{}, {}]".format(
+                        datetime.now(),
+                        thr_index,
+                        count,
+                        batch_size,
+                        ranges[thr_index][0],
+                        ranges[thr_index][1],
+                    )
+                )
         except ValueError:
             n_reading_error = n_reading_error + 1
-            print('Error reading {}:{}'.format(n_reading_error, filenames[idx]))
+            print("Error reading {}:{}".format(n_reading_error, filenames[idx]))
 
     writer.close()
     mean_batch.astype(np.float32).tofile(mean_filename_batch)
-    print('Thread {} --> saving mean at {}'.format(mean_filename_batch, thr_index))
+    print("Thread {} --> saving mean at {}".format(mean_filename_batch, thr_index))
     sys.stdout.flush()
 
 
-def create_tfrecords_threads(filenames, labels, image_shape, tfr_filename, process_function, n_threads):
+def create_tfrecords_threads(
+    filenames, labels, image_shape, tfr_filename, process_function, n_threads
+):
     assert len(filenames) == len(labels)
     # break whole dataset int batches according to the number of threads
     spacing = np.linspace(0, len(filenames), n_threads + 1).astype(np.int)
@@ -178,25 +200,37 @@ def create_tfrecords_threads(filenames, labels, image_shape, tfr_filename, proce
         ranges.append([spacing[i], spacing[i + 1]])
 
     # launch a thread for each batch
-    print('Launching {} threads for spacings: {}'.format(n_threads, ranges))
+    print("Launching {} threads for spacings: {}".format(n_threads, ranges))
     sys.stdout.flush()
     threads = []
     for thr_index in np.arange(len(ranges)):
-        args = (thr_index, ranges, filenames, labels, image_shape, tfr_filename, process_function)
+        args = (
+            thr_index,
+            ranges,
+            filenames,
+            labels,
+            image_shape,
+            tfr_filename,
+            process_function,
+        )
         t = threading.Thread(target=process_batch_threads, args=args)
         t.start()
         threads.append(t)
         # wait until all threads end
     for idx, thread in enumerate(threads):
         thread.join()
-    print('*******All threads have finished!!*******')
+    print("*******All threads have finished!!*******")
     # compute the mean image from those computed by each thread
     for idx in range(n_threads):
-        mean_filename = '{}_{}_mean.npy'.format(tfr_filename, idx)
+        mean_filename = "{}_{}_mean.npy".format(tfr_filename, idx)
         if idx == 0:
-            mean_image = np.reshape(np.fromfile(mean_filename, dtype=np.float32), image_shape)
+            mean_image = np.reshape(
+                np.fromfile(mean_filename, dtype=np.float32), image_shape
+            )
         else:
-            mean_image = mean_image + np.reshape(np.fromfile(mean_filename, dtype=np.float32), image_shape)
+            mean_image = mean_image + np.reshape(
+                np.fromfile(mean_filename, dtype=np.float32), image_shape
+            )
     mean_image = mean_image / n_threads
     return mean_image
 
@@ -212,17 +246,20 @@ def create_tfrecords(config, _type, processFun=imgproc.resize_image):
     image_shape = np.asarray(config.get_image_shape())
     n_threads = config.get_num_threads()
     # ------------- creating train data
-    if (_type == 'train') or (_type == 'all'):
-        filenames, labels = read_data_from_file(data_dir, dataset='train', shuf=True)
+    if (_type == "train") or (_type == "all"):
+        filenames, labels = read_data_from_file(data_dir, dataset="train", shuf=True)
         if config.use_multithreads():
-            tfr_filename = os.path.join(data_dir, 'train')
-            training_mean = create_tfrecords_threads(filenames, labels, image_shape, tfr_filename, processFun,
-                                                     n_threads)
+            tfr_filename = os.path.join(data_dir, "train")
+            training_mean = create_tfrecords_threads(
+                filenames, labels, image_shape, tfr_filename, processFun, n_threads
+            )
         else:
-            tfr_filename = os.path.join(data_dir, 'train.tfrecords')
-            training_mean = create_tfrecords_from_file(filenames, labels, image_shape, tfr_filename, processFun)
+            tfr_filename = os.path.join(data_dir, "train.tfrecords")
+            training_mean = create_tfrecords_from_file(
+                filenames, labels, image_shape, tfr_filename, processFun
+            )
 
-        print('train_record saved at {}.'.format(tfr_filename))
+        print("train_record saved at {}.".format(tfr_filename))
         # saving training mean
         mean_file = os.path.join(data_dir, "mean.dat")
         print("mean_file {}".format(training_mean.shape))
@@ -233,29 +270,41 @@ def create_tfrecords(config, _type, processFun=imgproc.resize_image):
         image_shape.astype(np.int32).tofile(shape_file)
         print("shape_file saved at {}.".format(shape_file))
         # -------------- creating test data
-    if (_type == 'test') or (_type == 'all'):
+    if (_type == "test") or (_type == "all"):
         filenames, labels = read_data_from_file(data_dir, dataset="test", shuf=True)
         if config.use_multithreads():
-            tfr_filename = os.path.join(data_dir, 'test')
-            create_tfrecords_threads(filenames, labels, image_shape, tfr_filename, processFun, n_threads)
+            tfr_filename = os.path.join(data_dir, "test")
+            create_tfrecords_threads(
+                filenames, labels, image_shape, tfr_filename, processFun, n_threads
+            )
         else:
             tfr_filename = os.path.join(data_dir, "test.tfrecords")
-            create_tfrecords_from_file(filenames, labels, image_shape, tfr_filename, processFun)
+            create_tfrecords_from_file(
+                filenames, labels, image_shape, tfr_filename, processFun
+            )
         print("test_record saved at {}.".format(tfr_filename))
 
     # parser tf_record to be used for dataset mapping
 
 
-def parser_tfrecord(serialized_input, input_shape, mean_image, number_of_classes, with_augmentation=False):
-    features = tf.io.parse_example([serialized_input],
-                                   features={
-                                       'image': tf.io.FixedLenFeature([], tf.string),
-                                       'label': tf.io.FixedLenFeature([], tf.int64)
-                                   })
+def parser_tfrecord(
+    serialized_input,
+    input_shape,
+    mean_image,
+    number_of_classes,
+    with_augmentation=False,
+):
+    features = tf.io.parse_example(
+        [serialized_input],
+        features={
+            "image": tf.io.FixedLenFeature([], tf.string),
+            "label": tf.io.FixedLenFeature([], tf.int64),
+        },
+    )
     # image
     # rgb_mean = [123.68, 116.779, 103.939]
     # rgb_std = [58.393, 57.12, 57.375]
-    image = tf.io.decode_raw(features['image'], tf.uint8)
+    image = tf.io.decode_raw(features["image"], tf.uint8)
     image = tf.reshape(image, input_shape)
     # data augmentation
     # central crop
@@ -268,7 +317,9 @@ def parser_tfrecord(serialized_input, input_shape, mean_image, number_of_classes
             # fraction = tf.random.uniform((), 0.5, 0.9, dtype = tf.float32)
             if prob < data_augmentation_prob * 0.5:
                 image = tf.image.central_crop(image, central_fraction=0.7)
-                image = tf.cast(tf.image.resize(image, (input_shape[0], input_shape[1])), tf.uint8)
+                image = tf.cast(
+                    tf.image.resize(image, (input_shape[0], input_shape[1])), tf.uint8
+                )
             else:
                 image = tf.image.flip_left_right(image)
 
@@ -279,7 +330,7 @@ def parser_tfrecord(serialized_input, input_shape, mean_image, number_of_classes
     image = image - mean_image
 
     # label
-    label = tf.cast(features['label'], tf.int32)
+    label = tf.cast(features["label"], tf.int32)
     label = tf.one_hot(label, depth=number_of_classes)
     label = tf.reshape(label, [number_of_classes])
 
@@ -288,16 +339,20 @@ def parser_tfrecord(serialized_input, input_shape, mean_image, number_of_classes
     # parser tf_record to be used for dataset mapping
 
 
-def parser_tfrecord_siamese(serialized_input, input_shape, mean_image, with_augmentation=False):
-    features = tf.io.parse_example([serialized_input],
-                                   features={
-                                       'image': tf.io.FixedLenFeature([], tf.string),
-                                       'label': tf.io.FixedLenFeature([], tf.int64)
-                                   })
+def parser_tfrecord_siamese(
+    serialized_input, input_shape, mean_image, with_augmentation=False
+):
+    features = tf.io.parse_example(
+        [serialized_input],
+        features={
+            "image": tf.io.FixedLenFeature([], tf.string),
+            "label": tf.io.FixedLenFeature([], tf.int64),
+        },
+    )
     # image
     # rgb_mean = [123.68, 116.779, 103.939]
     # rgb_std = [58.393, 57.12, 57.375]
-    image = tf.io.decode_raw(features['image'], tf.uint8)
+    image = tf.io.decode_raw(features["image"], tf.uint8)
     image = tf.reshape(image, input_shape)
     # data augmentation
     # central crop
@@ -310,7 +365,9 @@ def parser_tfrecord_siamese(serialized_input, input_shape, mean_image, with_augm
             #             #fraction = tf.random.uniform((), 0.5, 0.9, dtype = tf.float32)
             if prob < data_augmentation_prob * 0.5:
                 image = tf.image.central_crop(image, central_fraction=0.7)
-                image = tf.cast(tf.image.resize(image, (input_shape[0], input_shape[1])), tf.uint8)
+                image = tf.cast(
+                    tf.image.resize(image, (input_shape[0], input_shape[1])), tf.uint8
+                )
             else:
                 image = tf.image.flip_left_right(image)
 
@@ -321,5 +378,5 @@ def parser_tfrecord_siamese(serialized_input, input_shape, mean_image, with_augm
     image = image - mean_image
 
     # label
-    label = tf.cast(features['label'], tf.int32)
+    label = tf.cast(features["label"], tf.int32)
     return image, label
