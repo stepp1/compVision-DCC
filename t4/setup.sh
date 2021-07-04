64f4b8997d179be89004789d558dee55ed3e5b10
Stepp1 <sfaragg@gmail.com>
2021-07-04 18:43:04 -0400

    update in gitignore

--- t4/setup.sh
+++ t4/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install -y zip git gcc python3-opencv software-properties-common wget nano
+# apt-get install -y zip gcc python3-opencv software-properties-common wget nano
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh
 bash Miniconda3-py39_4.9.2-Linux-x86_64.sh -b -p ./miniconda
@@ -16,8 +16,6 @@ python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 pip install gdown opencv-python Pillow numpy matplotlib jupyterlab scipy shapely scikit-learn funcy
  
 gdown --id 1vkP2gj_lpZDMUU3a8TNRs4rRoJHsbiMd
-
-git clone https://github.com/stepp1/compVision-DCC.git
 unzip '*.zip' -d compVision-DCC/t4/data
 
 jupyter-notebook --no-browser -p=8080
